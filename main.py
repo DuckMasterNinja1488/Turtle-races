2eedab13fc6ec25d8c1232fd42924b3741bad521
DuckMasterNinja1488 <soglrep9992@gmail.com>
2026-08-31 11:59:38 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import turtle
 from time import sleep
 import random
+
+
 WIDTH = 500
 HEIGHT = 500
 COLORS = ['red','green','blue','orange','yellow','violet','black','silver','grey','gold']
+
+
 def get_number_of_racers():
     while True:
         racers = int(input("Введрите количество черепах для гонки от 2 до 10: "))
@@ -13,11 +17,15 @@ def get_number_of_racers():
         else:
             print(f"Выбранное количество гонщиков {racers}")
             return racers
+
+
 def init_turtle():
     screen = turtle.Screen()
     screen.setup(WIDTH,HEIGHT)
     screen.title('Черепашьи гонки')
     sleep(3)
+
+
 def create_turtles(colors):
     ready_racers= []
     spacing = WIDTH//(len(colors)+1)
@@ -31,6 +39,8 @@ def create_turtles(colors):
         t.pendown()
         ready_racers.append(t)
     return ready_racers
+
+
 def race(colors):
     turtles = create_turtles(colors)
     while True:
@@ -40,6 +50,8 @@ def race(colors):
             x, y = racer.pos()
             if y >= HEIGHT//2- 10:
                 return colors[turtles.index(racer)]
+
+            
 def main():
     racers = get_number_of_racers()
     init_turtle()
